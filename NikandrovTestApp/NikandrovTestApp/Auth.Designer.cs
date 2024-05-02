18e211d18a5a19c443c99fc9bf7d86d555d9f75b
Atstech <Atstech@gmail.com>
2024-05-02 19:54:21 +0300

    new test branch

---
+++ NikandrovTestApp/NikandrovTestApp/Auth.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace NikandrovTestApp
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoginComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PassTextBox = new System.Windows.Forms.TextBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.AuthButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин:";
+            // 
+            // LoginComboBox
+            // 
+            this.LoginComboBox.FormattingEnabled = true;
+            this.LoginComboBox.Location = new System.Drawing.Point(13, 34);
+            this.LoginComboBox.Name = "LoginComboBox";
+            this.LoginComboBox.Size = new System.Drawing.Size(350, 24);
+            this.LoginComboBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль:";
+            // 
+            // PassTextBox
+            // 
+            this.PassTextBox.Location = new System.Drawing.Point(13, 104);
+            this.PassTextBox.Name = "PassTextBox";
+            this.PassTextBox.Size = new System.Drawing.Size(350, 22);
+            this.PassTextBox.TabIndex = 3;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(13, 240);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(350, 47);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.Text = "Выход";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // AuthButton
+            // 
+            this.AuthButton.Location = new System.Drawing.Point(13, 184);
+            this.AuthButton.Name = "AuthButton";
+            this.AuthButton.Size = new System.Drawing.Size(350, 50);
+            this.AuthButton.TabIndex = 5;
+            this.AuthButton.Text = "Войти";
+            this.AuthButton.UseVisualStyleBackColor = true;
+            // 
+            // Auth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 299);
+            this.Controls.Add(this.AuthButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.PassTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LoginComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Auth";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox LoginComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PassTextBox;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button AuthButton;
+    }
+}
+
---
+++ NikandrovTestApp/NikandrovTestApp/Auth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dbconnect;
+
+namespace NikandrovTestApp
+{
+    public partial class Auth : Form
+    {
+        public Auth()
+        {
+            InitializeComponent();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(DB.connect())
+            {
+                MessageBox.Show("Yes");
+            } else MessageBox.Show("No");
+        }
+    }
+}
