a9f45eea6ba02e61c711e08fb26c271468d7e6b9
Atstech <Atstech@gmail.com>
2024-05-02 18:52:17 +0300

    first

---
+++ dbconnect/DB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace dbconnect
+{
+    class DB
+    {
+        static public string connectionstring = @"database=uslugi;datasource=localhost;user=root;pwd=1342";
+        
+        static public MySqlConnection myconnect;
+        static public MySqlCommand mycommand;
+        static public MySqlDataAdapter myadapter;
+
+        static public bool connect ()
+        {
+            try
+            {
+                myconnect = new MySqlConnection(connectionstring);
+                myconnect.Open();
+                mycommand = new MySqlCommand();
+                mycommand.Connection = myconnect;
+                myadapter = new MySqlDataAdapter();
+                return true;
+            } 
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
